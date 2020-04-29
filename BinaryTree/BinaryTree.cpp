3bc27b9d8f8360e802631d781833329f35eff0c1
Nookee411 <daniil43rus@list.ru>
2020-04-29 18:02:09 +0300

    Fixed tree

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
+#include <functional>
 
 
 using namespace std;
@@ -19,7 +20,6 @@ private:
     node* root = NULL;
 	void del(node&);
 	void ins(node&,node&);
-	void exactPull(node*, int);
 	bool exactSeatch(int, node*);
 	int exactDepth(node*);
 	string exactLeaves(node* current, string& res);
@@ -34,15 +34,24 @@ public:
       ~binaryTree();
 //    //Methods
       void push(T);
-      void pull(T);
       int depth();
 	  string leaves();
-      bool search(T);	  
+      bool search(T);
+	  bool remove(T value);
 };
 
 int main()
 {
-    
+	binaryTree<int> bt;
+	bt.push(3);
+	bt.push(4);
+	bt.push(2);
+	cout << "Depth: " << bt.depth()<<endl;
+	cout <<"- leaves"<< bt.leaves()<<endl;
+	cout <<"Is search working? "<< ((bt.search(3)) ? "ya" : "no");
+	bt.remove(3);
+	bt.remove(4);
+	bt.leaves();
 }
 
 template <typename T>
@@ -54,93 +63,53 @@ template<typename T>
 binaryTree<T>::~binaryTree()
 {
 	if (root != NULL)
-		del(root);
+		del(*root);
 };
 
 template <typename T>
 void binaryTree<T>::del(node& cur)
 {
-    if (cur->RightSib != NULL)
-        del(cur->RightSib);
-    if (cur->LeftSib != NULL)
-        del(cur->RightSib);
-    delete cur;
+	if (&cur == NULL)
+		return;
+    if (cur.RightSib != NULL)
+        del(*cur.RightSib);
+    if (cur.LeftSib != NULL)
+        del(*cur.LeftSib);
+    delete &cur;
 }
 
 template<typename T>
 void binaryTree<T>::ins(node&cur,node& temp)
 {
-	if (temp->value > cur->value)
+	if (temp.value > cur.value)
 	{
-		if (cur->RightSib != NULL)
-			ins(cur->RightSib, temp);
+		if (cur.RightSib != NULL)
+			ins(*cur.RightSib, temp);
 		else
-			cur -> RightSib = temp;
+			cur.RightSib = &temp;
 	}
 	else
 	{
-		if (cur->LeftSib != NULL)
-			ins(cur->LeftSib, temp);
+		if (cur.LeftSib != NULL)
+			ins(*cur.LeftSib, temp);
 		else
-			ins(cur->RightSib, temp);
+			cur.LeftSib = &temp;
 	}
 }
 
-template<typename T>
-void binaryTree<T>::exactPull(node* cur, int value)
-{
-	if (cur == NULL)
-		return;
-	else if (value < cur->value)
-	{
-		cur->LeftSib = exactPull(cur->LeftSib, value);
-	}
-	else if (value > cur->value)
-	{
-		cur->RightSib = exactPull(cur->RightSib, value);
-	}
-	else
-	{
-		if (cur->LeftSib != NULL)
-		{
-			node* temp = cur->RightSib;
-			delete cur;
-		}
-		else if (cur->RightSib != NULL)
-		{
-			node* temp = cur->LeftSib;
-			delete cur;
-		}
-		else
-		{
-			node* tempMax = cur;
-			while (tempMax->RightSib != NULL)
-				tempMax = tempMax->RightSib;
-			cur->LeftSib = exactPull(cur->LeftSib, tempMax->value);
-			cur->value = tempMax->value;
-		}
-	}
-}
+
 template <typename T>
 void binaryTree<T>::push(T value)
 {
 	node* temp = new node(value);
 	if (root == NULL)
 		root = temp;
-	if (search(value))
-		ins(root);
+	else if (!search(value))
+		ins(*root,*temp);
 }
 
 template<typename T>
-void binaryTree<T>::pull(T value)
-{
-	if (!search(value))
-		return;
-	else
-		exactPull(root, value);
-}
 
-template<typename T>
 int binaryTree<T>::depth()
 {
 	if (root != NULL)
@@ -163,7 +132,7 @@ bool binaryTree<T>::search(T value)
 	if (root == NULL)
 		return false;
 	else
-		return exactSeatch(root);
+		return exactSeatch(value,root);
 }
 
 
@@ -184,7 +153,7 @@ string binaryTree<T>::leaves()
 {
 	string res="";
 	if (root)
-		helper(root);
+		exactLeaves(root,res);
 	return res;
 }
 
@@ -192,10 +161,63 @@ string binaryTree<T>::leaves()
 template <typename T>
 string binaryTree<T>::exactLeaves(node* current,string& res)
 {
-		if (current->left_child)
-			exactLeaves(current->left_child,res);
-		if (current->right_child,res)
-			exactLeaves(current->right_child,res);
-		if (!current->left_child && !current->right_child,res)
-			res += current.value + " ";
+		if (current->LeftSib)
+			exactLeaves(current->LeftSib,res);
+		if (current->RightSib!=NULL)
+			exactLeaves(current->RightSib,res);
+		if (current->LeftSib==NULL && current->RightSib==NULL)
+			cout<< current->value<<' ';
+		return res;
+}
+
+template <typename T>
+bool binaryTree<T>::remove(T value)
+{
+	function<node*(node*, int)> helper = [&helper](node* current, int value) -> node*
+	{
+		if (!current)
+			return current;
+		else if (value < current->value)
+		{
+			current->LeftSib = helper(current->LeftSib, value);
+			return current;
+		}
+		else if (value > current->value)
+		{
+			current->RightSib = helper(current->RightSib, value);
+			return current;
+		}
+		else
+		{
+			if (!current->LeftSib)
+			{
+				node* temp = current->RightSib;
+				delete current;
+				return temp;
+			}
+			else if (!current->RightSib)
+			{
+				node* temp = current->LeftSib;
+				delete current;
+				return temp;
+			}
+			else
+			{
+				function<node* (node*)> find_max = [](node* cur) -> node*
+				{
+					while (cur->RightSib)
+						cur = cur->RightSib;
+					return cur;
+				};
+				node* max_node = find_max(current->LeftSib);
+				current->value = max_node->value;
+				current->LeftSib = helper(current->LeftSib, max_node->value);
+				return current;
+			}
+		}
+	};
+	if (!search(value))
+		return false;
+	helper(root, value);
+	return true;
 }
